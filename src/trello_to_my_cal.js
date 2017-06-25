c752ac6a9efc773dfb2e6e1dfddb9761c15cabb4
Haoyan Li <angelgamer@gmail.com>
2017-06-25 13:55:18 +0900

    Initial commit

---
+++ src/trello_to_my_cal.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Haoyan.Li on 2017/6/24.
+ */
+
+// ==UserScript==
+// @match      https://yourwebsite.com/xyz
+// @require    http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @require    http://cdnjs.cloudflare.com/ajax/libs/sugar/1.3/sugar.min.js
+// ==/UserScript==
+
+
+// <script src="https://apis.google.com/js/platform.js" async defer></script>
